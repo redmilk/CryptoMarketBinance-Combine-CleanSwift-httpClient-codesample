dbbb789cb0f98cab622e2790f8f43364cacd0b7b
Danyl Timofeyev <timofeev.danil@gmail.com>
2021-10-10 01:52:01 +0300

    Add all possible crypto symbols abreviations and available binance trading pairs

---
+++ VIPexample/Scenes/OrderBook/Binance/Symbols/QuoteAssets.swift
@@ -0,0 +1,38 @@
+//
+//  QuoteAssets.swift
+//  VIPexample
+//
+//  Created by Danyl Timofeyev on 10.10.2021.
+//
+
+import Foundation
+
+enum QuoteAsset: String {
+    case BIDR
+    case BKRW
+    case BNB
+    case BRL
+    case BTC
+    case BUSD
+    case BVND
+    case DAI
+    case ETH
+    case EUR
+    case GBP
+    case GYEN
+    case IDRT
+    case NGN
+    case PAX
+    case RUB
+    case TRX
+    case TRY
+    case TUSD
+    case UAH
+    case USDC
+    case USDP
+    case USDS
+    case USDT
+    case VAI
+    case XRP
+    case ZAR
+}
